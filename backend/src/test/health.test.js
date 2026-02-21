1aa667dea54fa3175b0d4525b33978c390d368fd
Evgeny Quest <evgenykvest@gmail.com>
2026-02-21 15:44:15 +0100

    Dependencies Installiert & Jest tests für /healthcheck und 404

--- backend/src/test/health.test.js
+++ backend/src/test/health.test.js
@@ -1,9 +1,23 @@
 import request from 'supertest';
 import app from '../app';
 
+
+// HealthCheck-Test
 describe('GET /health', () => {
     it('Muss 200 zurückgeben', async () => {
         const res = await request(app).get('/health');
+        
         expect(res.statusCode).toBe(200);
+        expect(res.body.ok).toBe(true);
+        expect(res.body.service).toBe('backend')
     });
 });
+
+// 404-Test
+describe('404 Handler', () => {
+    it('Muss Status:404 zurückgeben', async () => {
+        const res = await request(app).get('/unknown');
+
+        expect(res.statusCode).toBe(404)
+    })
+})
